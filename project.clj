e8e39b4792af6722c41c23cacc8dea6deaf70a17
Ramon Akhmetshin <eramonk@gmail.com>
2020-08-15 18:12:18 +0300

    WIP

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/clojurescript "1.10.764"]
                  [migratus "1.2.8"]
-                 [org.postgresql/postgresql "42.2.2"]            
+                 [org.postgresql/postgresql "42.2.2"]
                  [org.clojure/java.jdbc "0.7.11"]
                  [ring/ring-core "1.6.3"]
                  [ring/ring-jetty-adapter "1.6.3"]
@@ -14,37 +14,43 @@
                  [org.clojure/data.json "1.0.0"]
                  [selmer "1.12.28"]
                  [reagent "1.0.0-alpha2"]
-                 ]
+                 [cljs-ajax "0.8.0"]
+                 [reagent-forms "0.5.44"]]
   :plugins [[migratus-lein "0.7.3"]
-            [lein-figwheel "0.5.18"]
             [lein-ring "0.12.5"]
-            [lein-cprop "1.0.3"]
             [lein-cljsbuild "1.1.8"]]
-  :resource-paths ["resources" "target/cljsbuild"]
+  ;; :resource-paths ["resources" "target/cljsbuild"]
+  :resource-paths ["resources" "target/cljsbuild" "resources/public/js/compiled"]
   :target-path "target/%s/"
+
+  :clean-targets ^{:protect false} ["resources/public/js/compiled" "target" "test/js"]
   :cljsbuild
-  {:builds {:app {:source-paths ["src/cljs"]
-                  :compiler {:output-to  "target/cljsbuild/public/js/app.js"
-                             :output-dir "target/cljsbuild/public/js/out"
-                             :main "samurai-test-task.core"
-                             :asset-path "/js/out"
-                             :optimizations :none
-                             :source-map true
-                             :pretty-print true
-                             }}}}
-  :clean-targets ^{:protect false} [:target-path
-                                    [:cljsbuild :builds :app :compiler :output-dir]
-                                    [:cljsbuild :builds :app :compiler :output-to]]
+  {:builds [{:id "prod"
+             :source-paths ["src/cljs"]
+             :compiler {:output-to "resources/public/js/app.js"
+                        :optimizations :advanced
+                        :pretty-print false}}
+            {:id "dev"
+             :source-paths ["src/cljs"]
+             :compiler {:output-to "resources/public/js/compiled/app.js"
+                        :output-dir "resources/public/js/compiled/out"
+                        :main samurai-test-task.core
+                        :asset-path "/js/out"
+                        :optimizations :advanced}}]}
+
+  ;; :clean-targets ^{:protect false} [:target-path
+  ;;                                   [:cljsbuild :builds :app :compiler :output-dir]
+  ;;                                   [:cljsbuild :builds :app :compiler :output-to]]
+
   :ring {:handler ring.core/handler}
   :migratus {:store :database
-           :migration-dir "resources/migrations"
-           :db {:classname "com.mysql.jdbc.Driver"
-                :subprotocol "postgresql"
-                :subname "//localhost/postgresdb"
-                :user "admin"
-                :password "admin"}}
+             :migration-dir "resources/migrations"
+             :db {:classname "com.mysql.jdbc.Driver"
+                  :subprotocol "postgresql"
+                  :subname "//localhost/postgresdb"
+                  :user "admin"
+                  :password "admin"}}
   :repl-options {:init-ns clj.samurai-test-task.core}
-  :main clj.samurai-test-task.core
-  )
+  :main clj.samurai-test-task.core)
 
 
